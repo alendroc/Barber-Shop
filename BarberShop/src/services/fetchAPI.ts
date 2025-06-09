9e096b1e479c70cb2a307b3fe682b146651c18dd
JosueCR170 <josolorzano4@gmail.com>
2025-06-09 00:07:23 -0600

    login y register

--- BarberShop/src/services/fetchAPI.ts
+++ BarberShop/src/services/fetchAPI.ts
@@ -1,4 +1,4 @@
-const urlAPI = 'http://localhost:9001/graphql';
+const urlAPI = 'http://localhost:9001';
 
 export const fetchAPI = async (query: string, variables: object = {}, token?: string) => {
   const headers: any = {
@@ -6,7 +6,7 @@ export const fetchAPI = async (query: string, variables: object = {}, token?: st
   };
   if (token) headers.Authorization = `Bearer ${token}`;
 
-  const res = await fetch(urlAPI, {
+  const res = await fetch(`${urlAPI}/graphql`, {
     method: 'POST',
     headers,
     body: JSON.stringify({ query, variables }),
@@ -16,4 +16,27 @@ export const fetchAPI = async (query: string, variables: object = {}, token?: st
   if (errors) throw new Error(errors[0].message);
 
   return data;
+};
+
+
+export const login = async (email: string, password: string) => {
+  try {
+    const res = await fetch( `${urlAPI}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      throw new Error('Credenciales inválidas');
+    }
+
+    const data = await res.json();
+    return data.token;
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    throw error;
+  }
 };
