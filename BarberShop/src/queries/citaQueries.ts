62899e533b559d35c35e363ad9c9c981ec2e6771
JosueCR170 <josolorzano4@gmail.com>
2025-06-09 12:58:51 -0600

    agregar cita

--- BarberShop/src/queries/citaQueries.ts
+++ BarberShop/src/queries/citaQueries.ts
@@ -37,7 +37,7 @@ export const GET_CITAS = `
 `;
 
 export const CREATE_CITA = `
-  mutation CrearCita($input: CrearCitaInput!) {
+  mutation CrearCita($input: CitaInput!) {
     crearCita(input: $input) {
       id
       fecha
