dfe7474eeaf5fc010641526258df7403d5b9ce96
Pipe1844 <pipevifa@gmail.com>
2025-06-10 12:30:34 -0600

    eliminar imagenes

--- BarberShop/src/components/context/authContext.jsx
+++ BarberShop/src/components/context/authContext.jsx
@@ -3,14 +3,16 @@ import React, { createContext, useState, useEffect, useContext } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(sessionStorage.getItem('token') || null);
+  const [token, setToken] = useState(JSON.parse(sessionStorage.getItem('token')) || null);
 
   // Cuando token cambie, actualizar sessionStorage
   useEffect(() => {
     if (token) {
-      sessionStorage.setItem('token', token);
+      console.log("Token2", token)
+      sessionStorage.setItem('token', JSON.stringify(token));
     } else {
       sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
     }
   }, [token]);
 
